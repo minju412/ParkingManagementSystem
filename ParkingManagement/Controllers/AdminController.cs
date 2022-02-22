3ca78eb70c969ed2a93bfcbdf671d11fe9555fd1
minju412 <ln8847@ajou.ac.kr>
2022-02-22 10:35:55 +0900

    [#15] 타입별 입차 기능
    - car(부모), bus(자식), truck(자식) 세 종류
    
    - 입차시 차량 종류와 차량 번호 입력 받음
    
    - db에 car_type 지정

--- ParkingManagement/Controllers/AdminController.cs
+++ ParkingManagement/Controllers/AdminController.cs
@@ -6,7 +6,7 @@ namespace ParkingManagement.Controllers
 {
     public class AdminController : Controller
     {
-        // 관리자 - 주차장 수입 보기
+        // 입출차 기록 확인
         public ActionResult TotalTableList(int? page, string search)
         {
             int pageNum = (page ?? 1);
@@ -17,39 +17,22 @@ namespace ParkingManagement.Controllers
             return View(cars.ToPagedList(pageNum, listCount));
         }
 
-        //public ActionResult TotalTableList(string search)
-        //{
-        //    return View(Car.GetTotalList(search));
-        //}
-
+        // 주차장 수입 확인
         public ActionResult CheckFee(string search)
         {
             return View(CarModel.GetTotalList(search));
         }
 
+        // 회원 확인
         public ActionResult MemberList(int? page, string search)
         {
             int pageNum = (page ?? 1);
             int listCount = 5;
 
             var users = UserModel.GetUserList(search);
-
+            
             return View(users.ToPagedList(pageNum, listCount));
         }
-
-        //int CalcTotalFee(string search)
-        //{
-        //    int tot_fee = 0;
-
-        //    var cars = Car.GetTotalList(search);
-
-        //    foreach (var item in cars)
-        //    {
-        //        tot_fee += item.Parking_Fee;
-        //    }
-
-        //    return tot_fee;
-        //}
 
     }
 }
--- ParkingManagement/Controllers/HomeController.cs
+++ ParkingManagement/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using PagedList;
 using ParkingManagement.Models;
 using System;
+using System.Collections.Generic;
 using System.Web;
 using System.Web.Mvc;
 
@@ -33,11 +34,26 @@ namespace ParkingManagement.Controllers
         }
 
         [Authorize]
-        public ActionResult TableInsert_Input(string carnum)
+        public ActionResult TableInsert_Input(string carnum, string cartype)
         {
             try
             {
-                var model = new CarModel();
+                CarModel model;
+
+                switch (cartype)
+                {
+                    case "truck":
+                        model = new TruckModel();
+                        break;
+
+                    case "bus":
+                        model = new BusModel();
+                        break;
+
+                    default:
+                        model = new CarModel();
+                        break;
+                }
 
                 model.CarNum = carnum;
                 model.Owner_Name = User.Identity.Name;
@@ -80,76 +96,75 @@ namespace ParkingManagement.Controllers
                 {
                     throw new Exception("입차되지 않은 차량입니다");
                 }
-
-                // 권한 확인
-                if(model.Owner_Name != User.Identity.Name)
+                if (model.Owner_Name != User.Identity.Name)
                 {
                     throw new Exception("출차 권한이 없습니다.");
                 }
 
-                model.UpdateOutTime(); // db update - 출차시각 
-                model = CarModel.Get(carnum); // 출차 시각 업데이트된 모델 받음
+                model.UpdateOutTime(); 
 
-                int fee = CalcFee(carnum);
+                int fee = model.CalcFee(carnum);
 
-                model.UpdateFee(fee); // db update - 주차요금
+                model.UpdateFee(fee); 
                 model = CarModel.Get(carnum); // 주차요금 업데이트된 모델 받음
 
-                model.Delete(); // db delete
+                model.Delete();
 
-                // alert 후 redirect
                 return Content("<script> alert('차량번호: " + model.CarNum + " 출차합니다.\\n주차 요금은" + model.Parking_Fee.ToString() + "원 입니다.'); location.href = '/home/TableList'; </script>");
             }
             catch (Exception ex)
             {
-                // 실패
                 return Redirect($"/home/tabledelete?msg={HttpUtility.UrlEncode(ex.Message)}");
             }
         }
 
-        // 주차 요금 계산
-        int CalcFee(string carnum)
-        {
-            // 30분 미만 기본요금 1500원
-            int basic_min = 30;
-            int basic_fee = 1500;
 
-            // 추가 10분당 500원
-            int add_min = 10;
-            int add_fee = 500;
 
-            var model = CarModel.Get(carnum);
 
-            // 주차 시간 계산 (분으로 환산)
-            int min = (model.OutTime.Day * 24 * 60 + model.OutTime.Hour * 60 + model.OutTime.Minute) - (model.InTime.Day * 24 * 60 + model.InTime.Hour * 60 + model.InTime.Minute);
+        //[Authorize]
+        //public ActionResult TableDelete_Input(string carnum)
+        //{
+        //    try
+        //    {
+        //        var model = CarModel.Get(carnum);
 
-            // 주차 요금 계산
-            int fee = 0;
-            if (min >= basic_min)
-            {
-                fee += basic_fee;
-                min -= basic_min;
+        //        if (model == null)
+        //        {
+        //            throw new Exception("입차되지 않은 차량입니다");
+        //        }
 
-                if (min % add_min == 0)
-                {
-                    fee += add_fee * (min / add_min);
-                }
-                else
-                {
-                    fee += add_fee * (min / add_min) + add_fee;
-                }
-            }
-            else
-                fee = basic_fee;
+        //        // 권한 확인
+        //        if (model.Owner_Name != User.Identity.Name)
+        //        {
+        //            throw new Exception("출차 권한이 없습니다.");
+        //        }
 
-            return fee;
-        }
+        //        model.UpdateOutTime(); // db update - 출차시각 
+        //        //model = CarModel.Get(carnum); // 출차 시각 업데이트된 모델 받음 // 없어도 되는 듯..
+
+        //        int fee = model.CalcFee(carnum);
+
+        //        model.UpdateFee(fee); // db update - 주차요금
+        //        model = CarModel.Get(carnum); // 주차요금 업데이트된 모델 받음
+
+        //        model.Delete(); // db delete
+
+        //        // alert 후 redirect
+        //        return Content("<script> alert('차량번호: " + model.CarNum + " 출차합니다.\\n주차 요금은" + model.Parking_Fee.ToString() + "원 입니다.'); location.href = '/home/TableList'; </script>");
+        //        //return Content("<script> alert('차량번호: " + model.CarNum + " 출차합니다.\\n주차 시간은" + model.CalcMin(carnum).ToString() + "분 입니다.\\n주차 요금은" + model.Parking_Fee.ToString() + "원 입니다.'); location.href = '/home/TableList'; </script>");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // 실패
+        //        return Redirect($"/home/tabledelete?msg={HttpUtility.UrlEncode(ex.Message)}");
+        //    }
+        //}
 
         // 주차장 시설 확인
         public ActionResult ParkingView()
         {
             return View();
         }
-       
+
     }
 }
